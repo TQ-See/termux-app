ac03b55ba7e7129511d215f48d35365204632623
Tquilla. <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-04 15:42:48 +0700

    Update get_env.lua

--- .local/bin/LuaScripts/components/get_env.lua
+++ .local/bin/LuaScripts/components/get_env.lua
@@ -1,21 +1,22 @@
 local M = {}
 
-setmetatable(M, {
-    __call = function(_, path, global)
-        path = path or (os.getenv("HOME") .. "/.env/secrets.env")
+function M.load(path, global)
+    path = path or (os.getenv("HOME") .. "/.env/secrets.env")
 
-        local env = {}
-        for line in io.lines(path) do
-            local key, value = line:match("^([%w_]+)=(.*)$")
-            if key then
-                env[key] = value
-                if global then
-                    _G[key] = value
-                end
+    local env = {}
+
+    for line in io.lines(path) do
+        local key, value = line:match("^([%w_]+)=(.*)$")
+        if key then
+            env[key] = value
+
+            if global then
+                _G[key] = value
             end
         end
-        return env
     end
-})
+
+    return env
+end
 
 return M
