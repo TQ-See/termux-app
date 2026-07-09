254a41b38fa37d367b1a2f9f9b55489ea0d5616f
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-09 21:00:13 +0700

    Update via Auto Commit - 2026-07-09 21:00
    Update file: .config/yazi/init.lua
    
    Changes:
    old mode 100644
    new mode 100755
    ---
    ---
    +require("full-border"):setup()
    +require("starship"):setup()
    +require("restore"):setup()
    +require("zoxide"):setup({
    +	update_db = true,
    +})
    +
    +function Linemode:size_and_mtime()
    +	local time = math.floor(self._file.cha.mtime or 0)
    +	if time == 0 then
    +		time = ""
    +	elseif os.date("%Y", time) == os.date("%Y") then
    +		time = os.date("%b %d %H:%M", time)
    +	else
    +		time = os.date("%b %d  %Y", time)
    +	end
    +
    +	local size = self._file:size()
    +	return string.format("%s %s", size and ya.readable_size(size) or "dir", time)
    +end

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,12 +1,20 @@
--- require("full-border"):setup()
--- require("smart-enter"):setup {
--- 	open_multi = true,
--- }
---
--- Header:children_add(function()
--- 	if ya.target_family() ~= "unix" then
--- 		return ui.Line {}
--- 	end
--- 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("magenta")
--- end, 500, Header.LEFT)
---
+require("full-border"):setup()
+require("starship"):setup()
+require("restore"):setup()
+require("zoxide"):setup({
+	update_db = true,
+})
+
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "dir", time)
+end
